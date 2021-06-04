bd2068ae5b21c4283d5ebcea7303100f08e6fd25
masaya-kawamura <lucky.number0128@gmail.com>
2021-06-04 15:57:13 +0000

    [終了]Bookers2の本の一覧をいいねが多い順に並ぶように設定

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,7 @@ class Book < ApplicationRecord
 	belongs_to :user
 	has_many :favorites, dependent: :destroy
 	has_many :comments,  dependent: :destroy
+	has_many :favorite_users, through: :favorites, source: :user
 
 	def favorited_by?(user)
 		favorites.where(user_id: user.id).exists?
